54607cc99dee7f0e175f1d8e10c4426bf3b713c9
Sanusi Lamido <81477186+SanusiLamido@users.noreply.github.com>
2023-12-05 23:03:58 +0100

    refurbished

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-let Homebutton = document.getElementById("home")
-let MenuItem = document.getElementById(".navigation a")
-let menuActive = false;
+let popup = document.getElementById("popup");
+function openpopup() {
+  popup.classList.add("open-popup");
+};
 
-Homebutton.addEventListener("click", ()=>{
-    if (!menuActive){
-        MenuItem.style.transform = "translate(150px,0)";
-    }
-});
+function closepopup() {
+    popup.classList.remove("open-popup");
+  }
