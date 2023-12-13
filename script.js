e16a9889bd826dc4699cb720b2bb19b607abd089
Sanusi Lamido <81477186+SanusiLamido@users.noreply.github.com>
2023-12-13 15:45:31 +0100

    cleaned code

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-let popup = document.getElementById("popup");
-function openpopup() {
-  popup.classList.add("open-popup");
-};
+const popUp = document.getElementById("popup");
+const stackPopUp = document.getElementById("stackpopup");
+const mainItems = document.querySelectorAll(".main-item");
 
-function closepopup() {
-    popup.classList.remove("open-popup");
-  }
+function openpopup() {
+  popUp.classList.toggle("open-popup");
+}
+
+function stackopenpopup() {
+  stackPopUp.classList.toggle("open-stackpopup");
+}
+
+mainItems.forEach((mainItem) => {
+  mainItem.addEventListener("click", () => {
+    mainItem.classList.toggle("main-item--open");
+  });
+});
